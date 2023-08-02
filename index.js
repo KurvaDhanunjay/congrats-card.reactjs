393d8d3065daa8835b522a1ec56f4d3f647e241b
KurvaDhanunjay <kdhanunjay99@gmail.com>
2023-08-02 11:57:47 +0000

    congrats-card.reactjs

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="bg_container">
+    <h1 className="h">Congratulations</h1>
+    <div className="personCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>vishnu institute of computer education and technology bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
